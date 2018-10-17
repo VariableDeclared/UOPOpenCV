7f93525b73d177a4cfba17be89064455a6013306
Peter J De Sousa <petedes@live.com>
2018-10-17 11:39:00 +0100

    Exercise 2
    CPY and PASTE

--- week4/main.cpp
+++ week4/main.cpp
@@ -5,25 +5,24 @@ String samplePicturesPath = String(std::getenv("UNI_COMPUTER_VISION_DIR")) + "Sa
 
 int main()
 {
-
-	Mat img = imread(samplePicturesPath + "/building.jpg");
-	Mat imgHB = img + Scalar(75, 75, 75); //increase the brightness by 75 units
-     //img.convertTo(imgH, -1, 1, 75);
-	Mat imgLB = img + Scalar(-75, -75, -75); //decrease the brightness by 75 units
-     //img.convertTo(imgL, -1, 1, -75);
-	Mat imgHC, imgLC;
-	img.convertTo(imgHC, -1, 2, 0); //increase the contrast (double)
-	img.convertTo(imgLC, -1, 0.5, 0); //decrease the contrast (halve)
-	namedWindow("Original Image", CV_WINDOW_AUTOSIZE);
-	namedWindow("High Brightness", CV_WINDOW_AUTOSIZE);
-	namedWindow("Low Brightness", CV_WINDOW_AUTOSIZE);
-	namedWindow("High Contrast", CV_WINDOW_AUTOSIZE);
-	namedWindow("Low Contrast", CV_WINDOW_AUTOSIZE);
-	imshow("Original Image", img);
-	imshow("High Brightness", imgHB);
-	imshow("Low Brightness", imgLB);
-	imshow("High Contrast", imgHC);
-	imshow("Low Contrast", imgLC);
-	waitKey(0);
+	VideoCapture capture(samplePicturesPath + "/SampleVideo.mp4");
+	namedWindow("Original Video",CV_WINDOW_AUTOSIZE);
+	namedWindow("Brightness Increased",CV_WINDOW_AUTOSIZE);
+	namedWindow("Contrast Increased",CV_WINDOW_AUTOSIZE);
+	Mat frame, imgHB, imgHC;
+	int key = 0;
+	while(key != 27) // press "Esc" to stop
+	{
+		if (!capture.read(frame))
+		{
+			break;
+		}
+		imgHB = frame + Scalar(75, 75, 75); //increase the brightness by 75 units
+		frame.convertTo(imgHC, -1, 2, 0); //increase the contrast (double)
+		imshow("Original Video", frame);
+		imshow("Brightness Increased", imgHB);
+		imshow("Contrast Increased", imgHC);
+		key=waitKey(30);
+	}
 	return 0;
 }
