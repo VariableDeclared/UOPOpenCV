12fe94282397387302e8afb1a562da92ce9430a1
Peter J De Sousa <petedes@live.com>
2018-11-13 15:00:57 +0000

    Week 7 folder structure
    Copy of Week 6.

---
+++ week7/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "../include/cvtutorial.hpp"
+
+using namespace cv;
+// using namespace ComputerVision;
+using namespace std;
+
+void displayOpenCVVersion() {
+	std::cout << "OpenCV Version: " << std::endl << 
+		"v " << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION << std::endl;
+}
+
+
+
+String computerVisionProjectDir = String(std::getenv("UNI_COMPUTER_VISION_DIR"));
+
+
+bool compareRect(cv::Rect r1, cv::Rect r2) { return r1.height < r2.height; }
+
+int main()
+{
+	cvtutorial a;
+	a.FaceCollection(computerVisionProjectDir + "/faces/");
+
+}
