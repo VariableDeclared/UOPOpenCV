44b5336552f655f84eda3b2fd060fbfc529f5724
Peter J De Sousa <petedes@live.com>
2019-01-22 14:31:47 +0000

    Week 10 pt1.
    OpenGL example program

--- week11/src/main.cpp
+++ week11/src/main.cpp
@@ -1,64 +1,70 @@
-#include <opencv2/highgui/highgui.hpp>
-#include "dlib/image_processing/frontal_face_detector.h"
-#include "dlib/image_processing/render_face_detections.h"
-#include "dlib/image_processing.h"
-#include "dlib/opencv.h"
-#include "dlib/gui_widgets.h"
+#include <GL/glut.h>
+#include <stdlib.h>
 
+static GLfloat spin = 0.0;
 
-using namespace dlib;
-using namespace std;
-
-
-int main () {
-	cv::VideoCapture cap(0);
-
-	if(!cap.isOpened()) 
-	{
-		cerr << "Could not open video stream" << endl;
-		return 1;
-	}
-
-	image_window win; 
-
-	// Load face detection and pose estimation models.
-
-	frontal_face_detector detector = get_frontal_face_detector();
-	shape_predictor pose_model;
-
-	deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
-
-
-	// Grab and process frames until the main window is close by the user.
-
-
-	while(!win.is_closed())
-	{
-		//  Grab a frame
-		cv::Mat temp; 
-		cap >> temp;
-
-		cv_image<bgr_pixel> cimg(temp);
-		// Detect faces
-		std::vector<rectangle> faces = detector(cimg);
-
-		// Find the post of eah face.
-		double tstart = (double) cv::getTickCount(); 
-
-		std::vector<full_object_detection> shapes;
-		for(unsigned long i = 0 ; i< faces.size(); ++i)
-			shapes.push_back(pose_model(cimg, faces[i]));;
-
-		double time = ((double) cv::getTickCount() - tstart) / cv::getTickFrequency();
-		
-
-		//cout << time <<endl;
-
-		//display it all on the screen 
-		win.clear_overlay();
-		win.set_image(cimg); 
-		win.add_overlay(render_face_detections(shapes));
-	}
-
+void init(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_FLAT);
 }
 
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glRotatef(spin, 0.0, 0.0, 1.0);
+	glColor3f(0.0, 0.0, 1.0);
+	glRectf(-25.0, -25.0, 25.0, 25.0);
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void spinDisplay(void)
+{
+	spin = spin + 2.0;
+	if( spin > 360.0)
+		spin = spin - 360.0;
+	glutPostRedisplay();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-50.0, 50.0, -50.0, 50, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	switch(button) {
+		case GLUT_LEFT_BUTTON:
+			if (state == GLUT_DOWN)
+				glutIdleFunc(spinDisplay);
+			break;
+		case GLUT_RIGHT_BUTTON:
+			if(state == GLUT_DOWN)
+				glutIdleFunc(NULL);
+			break;
+		default:
+			break;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(250, 250);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
+	glutMainLoop();
+	return 0;
+}
